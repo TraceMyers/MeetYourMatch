f66ed5cff2abd6428a62900ed3d4d1d6af762be2
Trace Myers <18mm@protonmail.com>
2022-08-21 05:19:00 -0400

    edited recieve_clients and main heavily

--- turnserver.py
+++ turnserver.py
@@ -25,21 +25,16 @@ ERROR_CLIENT_STATE_BEHIND   = 0x80
 ERROR_CLIENT_STATE_AHEAD    = 0x90
 ERROR_BAD_STATUS            = 0xa0
 
-SORT_RPSI   = 0
-SORT_TFER   = 1
-SORT_ERR    = 2
-SORT_BAN    = 3
+SORT_REGI = 0
+SORT_PAIR = 1
+SORT_INIT = 2
+SORT_TFER = 3
+SORT_KICK = 4
 
 ROLE_CLIENT = 0
 ROLE_SERVER = 1
 ROLE_NONE   = 2
 
-ENTRY_TO_MAIN = 0
-TFER0_TO_MAIN = 1
-TFER1_TO_MAIN = 2
-MAIN_TO_ENTRY = 3
-MAIN_TO_TFER0 = 4
-MAIN_TO_TFER1 = 5
 
 # MAX_CONNECTS_PER_SEC = 
 
@@ -111,37 +106,37 @@ def session_init():
     pass
 
 
-def entry_process():
-   regi_t = Thread(target=register_clients) 
-   pair_t = Thread(target=pair_clients)
-   sess_t = Thread(target=session_init)
-
-
-def transfer_process(responses):
-    pass
+def handle_clients(pipe, responses):
+    client_data = pipe.recv()
 
 
 def receive_clients(
     udp_socket, 
     connection_data, 
-    entry_pipe,
-    tfer_pipes,
+    handler_pipes,
     responses, 
-    buffer_size
+    buffer_size,
+    subproc_ct
 ):
-    tfer_i = 0
+    pipe_i = -1
+
     while True:
+        pipe_i += 1
+        if pipe_i >= subproc_ct:
+            pipe_i = 0
+
         try:
             msg = udp_socket.recvfrom(buffer_size)
         except WindowsError: # probably incoming package too big; ignore
             continue
+
         msg_data = msg[0]
         ip_address = msg[1]
         remote_status = msg_data[0]
 
         if remote_status == STATUS_NONE:
             c = Client(name=msg_data[2:42], address=ip_address)
-            entry_pipe.send(c)
+            handler_pipes[pipe_i].send((c, SORT_REGI))
         else:
             address_port = ''.join(ip_address)
             try:
@@ -149,71 +144,74 @@ def receive_clients(
                 client.remote_status = remote_status
             except:
                 responses.put((ERROR_NO_CLIENT, ip_address))
+                continue
             if remote_status & STATUS_TRANSFER:
                 if client.local_status & STATUS_TRANSFER:
                     remote_tfer_state = msg_data[1]
                     if client.tfer_state == remote_tfer_state:
-                        tfer_pipes[tfer_i].send(client)
-                        tfer_i = 0 if tfer_i == 1 else 1
+                        handler_pipes[pipe_i].send((client, SORT_TFER))
                     elif client.tfer_state > remote_tfer_state:
                         responses.put((ERROR_CLIENT_STATE_BEHIND, ip_address))
+                        continue
                     else:
                         responses.put((ERROR_CLIENT_STATE_AHEAD, ip_address))
+                        continue
                 responses.put((ERROR_BAD_STATUS, ip_address))
             elif remote_status == STATUS_PAIRING:
-                if client.local_status == STATUS_PAIRING \
-                or client.local_status == STATUS_SESSION_PREPARING:
-                    entry_pipe.send(client)
-                responses.put((ERROR_BAD_STATUS, ip_address))
+                if client.local_status == STATUS_PAIRING:
+                    handler_pipes[pipe_i].send((client, SORT_PAIR))
+                elif client.local_status == STATUS_SESSION_PREPARING:
+                    handler_pipes[pipe_i].send((client, SORT_INIT))
+                else:
+                    responses.put((ERROR_BAD_STATUS, ip_address))
+                    continue
             elif remote_status == STATUS_SESSION_PREPARING:
                 if client.local_status == STATUS_SESSION_PREPARING:
-                    entry_pipe.send(client)
-                responses.put((ERROR_BAD_STATUS, ip_address))
+                    handler_pipes[pipe_i].send(client)
+                else:
+                    responses.put((ERROR_BAD_STATUS, ip_address))
+                    continue
             elif remote_status == STATUS_SESSION_READY:
                 if client.local_status == STATUS_SESSION_PREPARING \
                 or client.local_status == STATUS_SESSION_READY:
-                    entry_pipe.send(client)
+                    handler_pipes[pipe_i].send(client)
                 elif client.local_status == STATUS_TRANSFER:
-                    tfer_pipes[tfer_i].send(client)
-                    tfer_i = 0 if tfer_i == 1 else 1
-                responses.put((ERROR_BAD_STATUS, ip_address))
+                    handler_pipes[pipe_i].send(client)
+                else:
+                    responses.put((ERROR_BAD_STATUS, ip_address))
+                    continue
 
+# TODO: figure out how to pack out data, including where ROLE goes
 
 def main():
     local_ip = '192.168.0.203'
     local_port = 8192
     buffer_size = 1024
+    subproc_ct = 3
     connection_data = ConnectionData()
 
     udp_socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     udp_socket.bind((local_ip, local_port))
     print(f'UDP TURN server running on {local_ip}:{local_port}')
 
-    entry_pipe_remote, entry_pipe_local = Pipe()
-    tfer0_pipe_remote, tfer0_pipe_local = Pipe()
-    tfer1_pipe_remote, tfer1_pipe_local = Pipe()
+    handler_pipes = (Pipe() for _ in range(subproc_ct))
     responses = Queue()
 
-    entry_args = (entry_pipe_remote, responses)
-    tfer0_args = (tfer0_pipe_remote, responses)
-    tfer1_args = (tfer1_pipe_remote, responses)
+    handler_args = ((handler_pipes[i], responses) for i in range(subproc_ct))
     reccl_args = (
         udp_socket, 
         connection_data, 
-        entry_pipe_local,
-        (tfer0_pipe_local, tfer1_pipe_local),
+        handler_pipes,
         responses, 
-        buffer_size
+        buffer_size,
+        subproc_ct
     )
 
-    entry_proc = Process(target=entry_process, args=entry_args)
-    tfer0_proc = Process(target=transfer_process, args=tfer0_args)
-    tfer1_proc = Process(target=transfer_process, args=tfer1_args)
+    handlers = [Process(target=handle_clients, args=handler_args[i]) for i in range(subproc_ct)]
     reccl_t = Thread(target=receive_clients, args=reccl_args)
     try:
-        entry_proc.start()
-        tfer0_proc.start()
-        tfer1_proc.start()
+        for i in range(subproc_ct):
+            handlers[i].start()
         reccl_t.start()
         while True:
             outgoing = []
